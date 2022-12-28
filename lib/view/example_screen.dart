97726326eca3c717898ee4e9a773e2ffb0127e31
lysokheng <sokheng6485@gmail.com>
2022-12-28 15:53:04 +0700

    init project

---
+++ lib/view/example_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ExampleScreen extends StatelessWidget {
+  const ExampleScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('hello'));
+  }
+}
