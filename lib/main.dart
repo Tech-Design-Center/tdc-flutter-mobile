5b3ce2f035b7be6135f0ddb673e38517ba1450f0
lysokheng <sokheng6485@gmail.com>
2023-01-18 18:02:34 +0700

    Set Up MVC, Test Call API Endpoint, and much more

--- lib/main.dart
+++ lib/main.dart
@@ -2,33 +2,62 @@ import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import 'package:tdc_frontend_mobile/view/google_translator_screen.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:tdc_frontend_mobile/route/app_page.dart';
+import 'package:tdc_frontend_mobile/route/app_route.dart';
+import 'package:tdc_frontend_mobile/theme/app_theme.dart';
 
-void main() {
+import 'model/ad_banner.dart';
+import 'model/category.dart';
+import 'model/user.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+
+  //register adapters
+  Hive.registerAdapter(AdBannerAdapter());
+  Hive.registerAdapter(CategoryAdapter());
+  Hive.registerAdapter(UserAdapter());
+
+  configLoading();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
       designSize: const Size(375, 667),
       minTextAdapt: true,
-      splitScreenMode: true,
       builder: (BuildContext context, Widget? child) {
         return GetMaterialApp(
-          builder: EasyLoading.init(),
-          title: 'TDC',
-          theme: ThemeData(
-            primarySwatch: Colors.blue,
-          ),
-          home: ExampleScreen(),
+          getPages: AppPage.list,
+          initialRoute: AppRoute.dashboard,
           debugShowCheckedModeBanner: false,
+          theme: AppTheme.lightTheme,
+          themeMode: ThemeMode.light,
+          builder: EasyLoading.init(),
         );
       },
     );
   }
+}
+
+void configLoading() {
+  EasyLoading.instance
+    ..displayDuration = const Duration(milliseconds: 2000)
+    ..indicatorType = EasyLoadingIndicatorType.fadingCircle
+    ..loadingStyle = EasyLoadingStyle.dark
+    ..indicatorSize = 45.0
+    ..radius = 10.0
+    ..progressColor = Colors.white
+    ..backgroundColor = Colors.green
+    ..indicatorColor = Colors.white
+    ..textColor = Colors.white
+    ..userInteractions = false
+    ..maskType = EasyLoadingMaskType.black
+    ..dismissOnTap = true;
 }
