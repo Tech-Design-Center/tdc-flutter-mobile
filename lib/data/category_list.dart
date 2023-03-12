f79e372b0043bf443ea822a6ed2d685a2b973ce4
Mekmun Sopheaktra <Mekmunsopheaktra@gmail.com>
2023-03-12 22:49:29 +0700

    home

---
+++ lib/data/category_list.dart
@@ -0,0 +1,14 @@
+ import 'package:mystic_academy/models/image_tilte_model.dart';
+
+import '../core/app_export.dart';
+
+List<ImageTitleModel> categoryList=[
+    ImageTitleModel(img: ImageConstant.category1, title: "IT & Software"),
+    ImageTitleModel(img: ImageConstant.category2, title: "UIUX Design"),
+    ImageTitleModel(img: ImageConstant.category3, title: "Graphic"),
+    ImageTitleModel(img: ImageConstant.category4, title: "Marketing"),
+    ImageTitleModel(img: ImageConstant.category5, title: "Fashion"),
+    ImageTitleModel(img: ImageConstant.category6, title: "Photography"),
+    ImageTitleModel(img: ImageConstant.category7, title: "Art"),
+
+  ];
