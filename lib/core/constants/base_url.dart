3579f2adfef90322f6fe1f1f5605620e6a1953bb
lysokheng <sokheng6485@gmail.com>
2023-05-11 22:57:06 +0700

    Build IOS APP & Change Icon App

--- lib/core/constants/base_url.dart
+++ lib/core/constants/base_url.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
 
-String baseUrl = Platform.isAndroid ? 'https://tdc-render-hosting.onrender.com' : 'http://localhost:1337';
+String baseUrl = Platform.isAndroid
+    ? 'https://tdc-render-hosting.onrender.com'
+    : 'https://tdc-render-hosting.onrender.com';
 //  String baseUrl = 'https://tdc-render-hosting.onrender.com';
 //     Platform.isAndroid ? 'https://0000000000000000000000000' : 'http://192.168.1.5:1338';
